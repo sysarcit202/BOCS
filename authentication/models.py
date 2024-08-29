63557fdb01e1bf92c7b70c0c4b9682022d4018ef
mark john kilapkilap <markjohnkilapkilap1@gmail.com>
2024-08-29 14:43:06 +0800

    Second Blood

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class CustomUser(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
--- authentication/views.py
+++ authentication/views.py
@@ -1,36 +1,60 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.contrib import messages
+
 from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def home (request):
     context = {
-        'title': 'Project | BOCS'
+        'title': 'Project_BOCS'
+    }
+
+    return render(request, 'authentication/index.html', context)
+
+def signup(request):
+    context = {
+        'title': 'Project_BOCS | Signup'
     }
 
     if request.method == 'POST':
-        username = request.POST['username']
-        pass1 = request.POST['pass1']
+        username = request.POST['Uname']
+        email = request.POST['Uemail']
+        pass1 = request.POST['Upass']
+        pass2 = request.POST['Urpass']
         
-        user = authenticate(username=username, password=pass1)
-        
-        if user is not None:
-            login(request, user)
-            fname = user.first_name
-            # messages.success(request, "Logged In Sucessfully!!")
-            return render(request, "authentication/index.html",{"fname":fname})
-        else:
-            messages.error(request, "Bad Credentials!!")
-            return redirect('home')
-        
-    return render(request, 'authentication/index.html', context)
+        user = User.objects.create_user(username, email, pass1)
+        user.fname = 'Ufname'
+        user.lname = 'Ulname'
 
-def login(request):
-    return render(request, 'authentication/dashboard.html')
+        user.save()
+            
+        messages.success(request, 'Your account has been successfully created.')
 
-def signup(request):
-    return render(request, 'authentication/signup.html')
+        return redirect('')
+
+        # user = authenticate(username=username, password=pass1)
+        
+        # if user is not None:
+        #     login(request, user)
+        #     fname = user.first_name
+        #     # messages.success(request, "Logged In Sucessfully!!")
+        #     return render(request, "authentication/index.html",{"fname":fname})
+        # else:
+        #     messages.error(request, "Bad Credentials!!")
+        #     return redirect('home')
+    
+    return render(request, 'authentication/signup.html', context)
+
+def dashboard(request):
+    context = {
+        'title': 'Project_BOCS | Dashboard'
+    }
+
+    return render(request, 'authentication/dashboard.html', context)
 
 def signout(request):
     # return render(request, 'authentication/signout.html')
     pass
+
